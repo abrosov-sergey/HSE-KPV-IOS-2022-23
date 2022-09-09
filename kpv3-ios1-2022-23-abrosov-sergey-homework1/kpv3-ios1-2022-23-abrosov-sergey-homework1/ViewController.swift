a45452bd48d89365c89f58f2335eda57bbb60d70
Sergey Abrosov <sgabrosov@yandex.ru>
2022-09-10 00:05:22 +0300

    First commit

---
+++ kpv3-ios1-2022-23-abrosov-sergey-homework1/kpv3-ios1-2022-23-abrosov-sergey-homework1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kpv3-ios1-2022-23-abrosov-sergey-homework1
+//
+//  Created by Сергей Абросов on 10.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
